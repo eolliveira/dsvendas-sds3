27315c564304fe15bdfbeb3ba3ffed78542771aa
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-11-13 19:55:52 -0300

    BartChart Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
-  }
+  sellerName: string,
+  sum: number
+}
+
+export type SaleSucces = {
+  "sellerName": "Logan",
+  "visited": 1495,
+  "deals": 684
+}
