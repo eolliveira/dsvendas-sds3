32f2b68f20997b3111fa1ec3cc9391714d016926
Erick Oliveira <erickoliveira@rorato.local>
2021-12-08 07:59:55 -0300

    heroku backend integration 2

--- frontend/src/utils/Requests.ts
+++ frontend/src/utils/Requests.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL = 'https://erick-curso-sds3.herokuapp.com/' ?? "http://localhost:8080"
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL = 'https://erick-curso-sds3.herokuapp.com' ?? "http://localhost:8080"
